f3702faaba35b25bd93ac0b628e3b223ecc7c725
Diego Imbert <imbert.diego05000@gmail.com>
2021-05-25 21:28:23 +0200

    changed jest test environment

--- apps/backend/jest.config.js
+++ apps/backend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   displayName: 'backend',
   preset: '../../jest.preset.js',
+  testEnvironment: 'node',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
