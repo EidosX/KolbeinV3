fa64f1704ae2b5066001b72b3f104bcd9d4c857c
Diego Imbert <imbert.diego05000@gmail.com>
2021-05-26 11:08:21 +0200

    moved session module ownership to auth module

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { TwitchBotModule } from './twitch-bot/twitch-bot.module';
-import { SessionModule } from './auth/session/session.module';
+import { AuthModule } from './auth/auth.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DB_CONNECTION_STRING),
     UserModule,
     TwitchBotModule,
-    SessionModule,
+    CryptoModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
