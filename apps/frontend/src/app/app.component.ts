5e112235e292ca0c1abca0d48963ab58d9116fea
Diego Imbert <imbert.diego05000@gmail.com>
2021-05-27 12:36:37 +0200

    scss to sass

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'kolbein-v3-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {}
