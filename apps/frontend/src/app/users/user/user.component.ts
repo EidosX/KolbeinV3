64432cad93c5499b38c2e3ef46bcce0763a57cb5
Diego Imbert <imbert.diego05000@gmail.com>
2021-05-30 19:33:50 +0200

    Linked most of user page infos to backend

--- apps/frontend/src/app/users/user/user.component.ts
+++ apps/frontend/src/app/users/user/user.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PublicUser } from '@interfaces/dto/user.dto';
+import { Observable } from 'rxjs';
 import { UsersService } from '../users.service';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'kolbein-v3-user',
@@ -7,8 +11,33 @@ import { UsersService } from '../users.service';
   styleUrls: ['./user.component.sass'],
 })
 export class UserComponent {
-  constructor(readonly usersService: UsersService) {}
+  constructor(
+    readonly usersService: UsersService,
+    readonly route: ActivatedRoute
+  ) {}
   section: Section = 'general';
+  user$: Observable<PublicUser> = this.route.params.pipe(
+    switchMap(({ userId }) => this.usersService.fetchUser(userId))
+  );
+
+  rankStr$: Observable<string> = this.user$.pipe(
+    map((u) => {
+      switch (u.rank) {
+        case 'user':
+          return 'Utilisateur';
+        case 'mod':
+          return 'Modérateur';
+        case 'admin':
+          return 'Administrateur';
+        case 'dev':
+          return 'Developpeur';
+      }
+    })
+  );
+  soundcloud$ = this.user$.pipe(map((u) => u.socials?.soundcloud));
+  youtube$ = this.user$.pipe(map((u) => u.socials?.youtube));
+  discord$ = this.user$.pipe(map((u) => u.socials?.discord));
+  twitch$ = this.user$.pipe(map((u) => u.socials?.twitch));
 }
 
 type Section = 'general' | 'socials' | 'challenges' | 'actions';
